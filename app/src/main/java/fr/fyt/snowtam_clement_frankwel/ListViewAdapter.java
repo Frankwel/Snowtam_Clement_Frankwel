234c0cefa1aead00e8c2d31743ac479ed697a96e
frank <frankwely@yahoo.fr>
2017-11-14 12:31:42 +0100

    Premier dépôt

---
+++ app/src/main/java/fr/fyt/snowtam_clement_frankwel/ListViewAdapter.java
@@ -0,0 +1,36 @@
+package fr.fyt.snowtam_clement_frankwel;
+
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.widget.ArrayAdapter;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by frank on 14/11/2017.
+ * this class will be used to communique with ListView
+ */
+
+public class ListViewAdapter extends ArrayAdapter<String> {
+
+    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+    public ListViewAdapter(@NonNull Context context, @LayoutRes int resource, @NonNull List<String> objects) {
+        super(context, resource, objects);
+        for (int i = 0; i < objects.size(); i++) {
+            mIdMap.put(objects.get(i), i);
+        }
+    }
+    @Override
+    public long getItemId(int position){
+        String item = getItem(position);
+        return mIdMap.get(item);
+    }
+
+    @Override
+    public boolean hasStableIds(){
+        return true;
+    }
+}
