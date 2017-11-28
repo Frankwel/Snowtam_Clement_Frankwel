232e85eb488de0af98ac3c798dbbe0647bbbbc13
clement-chollet <clement.chollet24@gmail.com>
2017-11-28 12:56:12 +0100

    delete backbutton in mapsActivity2

--- app/src/main/java/fr/fyt/snowtam_clement_frankwel/activity/MapsActivity.java
+++ app/src/main/java/fr/fyt/snowtam_clement_frankwel/activity/MapsActivity.java
@@ -37,6 +37,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         switchMap = (Button)findViewById(R.id.switchMap);
 
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
